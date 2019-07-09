34183895ccbde2f6c061ba4ec8796d866d209e5e
Alexthe666 <alex.rowlands@cox.net>
2019-07-09 00:25:50 +0000

    git file deleted, here was my changes

--- 2DGraphicsModeler/mainwindow.cpp
+++ 2DGraphicsModeler/mainwindow.cpp
@@ -6,6 +6,18 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+     QPixmap quitIcon("quit.png");
+     QAction *quitAction = new QAction(quitIcon, "&Quit", this);
+     QAction *saveAction = new QAction("&Save", this);
+     QAction *openAction = new QAction("&Open", this);
+     quitAction->setShortcut(Qt::CTRL + Qt::Key_Q);
+     saveAction->setShortcut(Qt::CTRL + Qt::Key_S);
+     openAction->setShortcut(Qt::CTRL + Qt::Key_O);
+     QMenu *file = menuBar()->addMenu("File");
+     file->addAction(quitAction);
+     file->addAction(saveAction);
+     file->addAction(openAction);
+     connect(quitAction, &QAction::triggered, qApp, &QApplication::quit);
 }
 
 MainWindow::~MainWindow()
