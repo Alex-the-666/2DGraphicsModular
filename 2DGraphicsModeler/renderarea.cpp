a7a1e4043c24120c8adef886f4bffb7d32fb3f25
oliveryue2-Arthur Siu <51488740+oliveryue2@users.noreply.github.com>
2019-07-08 22:11:21 -0700

    updated the size of windows so it doesn't freak people out
    Because I freaked out for a second.

--- 2DGraphicsModeler/renderarea.cpp
+++ 2DGraphicsModeler/renderarea.cpp
@@ -4,6 +4,8 @@ const QSize minSize(1000,500);
 RenderArea::RenderArea(QWidget *parent) : QWidget(parent)
 {
     setMinimumSize(minSize);
+    setBackgroundRole(QPalette::Base);
+    setAutoFillBackground(true);
 }
 
 void RenderArea::paintEvent(QPaintEvent *event)
