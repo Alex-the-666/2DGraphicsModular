3e11ee578ab6e43f748c7a9ac71c0ba0901f9353
Alexthe666 <alex.rowlands@cox.net>
2019-07-10 17:05:01 -0700

    save and load functionality

--- 2DGraphicsModeler/mainwindow.cpp
+++ 2DGraphicsModeler/mainwindow.cpp
@@ -1,5 +1,7 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QFileDialog>
+#include <QString>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -28,15 +30,22 @@ void MainWindow::on_TestAddShape_released()
 
 void MainWindow::on_actionOpen_triggered()
 {
-
+    QString filePath=QFileDialog::getOpenFileName(this, "save.txt");
+    QDir d = QFileInfo(filePath).absoluteDir();
+    QString absolute=d.absolutePath();
+    //todo implement load all shapes from QFileInfo
 }
 
 void MainWindow::on_actionSave_triggered()
 {
-
+    QString path = QFileDialog::getSaveFileName(nullptr, tr("Save"), ".txt");
+    QFile file(path);
+    file.open(QIODevice::WriteOnly);
+    //todo write entire shape vector to QFile
+    file.write("testing");
 }
 
 void MainWindow::on_actionQuit_triggered()
 {
-
+    QApplication::quit();
 }
