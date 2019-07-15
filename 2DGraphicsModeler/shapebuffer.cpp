2581151b98e06f02a6e4076eb590dc4447b519b5
oliveryue2-Arthur Siu <51488740+oliveryue2@users.noreply.github.com>
2019-07-14 21:45:12 -0700

    Shape buffer file stream update
    Created methods to convert strings into QT enum types.

--- 2DGraphicsModeler/shapebuffer.cpp
+++ 2DGraphicsModeler/shapebuffer.cpp
@@ -30,6 +30,7 @@ void ShapeBuffer::defaultStyle()
 
 void ShapeBuffer::readIn(QTextStream&)
 {
+    try{
     QString path = "D:/test.txt";
     QString string1 = "ShapeId: 1";
     QString string3 = "Line";
@@ -47,23 +48,154 @@ void ShapeBuffer::readIn(QTextStream&)
     for (int i= 1; i < list.size(); i++)
     {
         out << list.at(i).toInt();
-    }
+   }
   */
-    if (string3.contains("Line"))
-        shape = LINE;
-    if (string3.contains("Polyline"))
-        shape = POLYLINE;
-    if (string3.contains("Polygon"))
-        shape = POLYGON;
-    if (string3.contains("Square"))
-        shape = SQUARE;
-    if (string3.contains("Rectangle"))
-        shape = RECTANGLE;
-    if (string3.contains("Ellipse"))
-        shape = ELLIPSE;
-    if (string3.contains("Circle"))
-        shape = CIRCLE;
-    if (string3.contains("Text"))
-        shape = TEXT;
+    switch (shape)
+    {
+    case LINE:break;
+    case POLYLINE: break;
+    case POLYGON: break;
+    case RECTANGLE: break;
+    case SQUARE: break;
+    case ELLIPSE: break;
+    case CIRCLE: break;
+    case TEXT: break;
+    }
+
+    }
+
+
+    catch(MyException){
+
+    }
 
 }
+
+ShapeType ShapeBuffer::setShape(QString& string3)const
+{    if (string3.contains("Line"))
+        return LINE;
+    else if (string3.contains("Polyline"))
+        return POLYLINE;
+    else if (string3.contains("Polygon"))
+        return POLYGON;
+    else if (string3.contains("Square"))
+        return SQUARE;
+    else if (string3.contains("Rectangle"))
+        return RECTANGLE;
+    else if (string3.contains("Ellipse"))
+        return ELLIPSE;
+    else if (string3.contains("Circle"))
+        return CIRCLE;
+    else if (string3.contains("Text"))
+        return TEXT;
+    else
+        throw MyException();
+}
+
+Qt::PenStyle ShapeBuffer::setPenStyle(QString& x) const
+{
+    if(x.contains("NoPen"))
+        return Qt::NoPen;
+    else if(x.contains("SolidLine"))
+        return Qt::SolidLine;
+    else if(x.contains("DashLine"))
+        return Qt::DashLine;
+    else if(x.contains("DotLine"))
+        return Qt::DotLine;
+    else if(x.contains("DashDotLine"))
+        return Qt::DashDotLine;
+    else if(x.contains("DashDotDotLine"))
+        return Qt::DashDotDotLine;
+    else
+        throw MyException();
+}
+
+Qt::PenCapStyle ShapeBuffer::setPenCapStyle(QString& x) const
+{
+    if(x.contains("FlatCap"))
+        return Qt::FlatCap;
+    else if(x.contains("SquareCap"))
+        return Qt::SquareCap;
+    else if(x.contains("RoundCap"))
+        return Qt::RoundCap;
+    else
+        throw MyException();
+}
+
+Qt::PenJoinStyle ShapeBuffer::setPenJoinStyle(QString& x) const
+{
+    if(x.contains("MiterJoin"))
+        return Qt::MiterJoin;
+    else if(x.contains("BevelJoin"))
+        return Qt::BevelJoin;
+    else if(x.contains("RoundJoin"))
+        return Qt::RoundJoin;
+    else
+        throw MyException();
+}
+
+Qt::GlobalColor ShapeBuffer::setColor(QString& x) const
+{
+    if(x.contains("white"))
+        return Qt::white;
+    else if(x.contains("black"))
+        return Qt::black;
+    else if(x.contains("red"))
+        return Qt::red;
+    else if(x.contains("green"))
+        return Qt::green;
+    else if(x.contains("blue"))
+        return Qt::blue;
+    else if(x.contains("cyan"))
+        return Qt::cyan;
+    else if(x.contains("magenta"))
+        return Qt::magenta;
+    else if(x.contains("yellow"))
+        return Qt::yellow;
+    else if(x.contains("gray"))
+        return Qt::gray;
+    else
+        throw MyException();
+
+}
+
+Qt::BrushStyle ShapeBuffer::setBrushStyle(QString & x) const
+{
+    if(x.contains("SolidPattern"))
+        return Qt::SolidPattern;
+    else if(x.contains("HorPattern"))
+        return Qt::HorPattern;
+    else if(x.contains("VerPattern"))
+        return Qt::VerPattern;
+    else if(x.contains("NoBrush"))
+        return Qt::NoBrush;
+    else
+        throw MyException();
+}
+
+QFont::Style ShapeBuffer::getTextFontStyle(QString & x) const
+{
+    if(x.contains("StyleNormal"))
+        return QFont::StyleNormal;
+    else if(x.contains("StyleItalic"))
+        return QFont::StyleItalic;
+    else if(x.contains("StyleOblique"))
+        return QFont::StyleOblique;
+    else
+        throw MyException();
+}
+
+QFont::Weight ShapeBuffer::getTextFontWeight(QString & x) const
+{
+    if(x.contains("Thin"))
+        return QFont::Thin;
+    else if(x.contains("Normal"))
+        return QFont::Normal;
+    else if(x.contains("Bold"))
+        return QFont::Bold;
+    else
+        throw MyException();
+}
+
+
+
--- 2DGraphicsModeler/shapebuffer.h
+++ 2DGraphicsModeler/shapebuffer.h
@@ -10,6 +10,8 @@
 
 enum ShapeType{LINE, POLYLINE, POLYGON, RECTANGLE,\
                SQUARE, ELLIPSE, CIRCLE, TEXT};
+class MyException
+{};
 
 class ShapeBuffer
 {
@@ -17,13 +19,26 @@ class ShapeBuffer
 public:
     ShapeBuffer();
 
-
     void reset();
     void defaultStyle();
 
     void readIn(QTextStream&);
-
     //buffer variables
+
+protected:
+    ShapeType setShape(QString&) const;
+    //Pen Width
+    Qt::PenStyle setPenStyle(QString&)const;
+    Qt::PenCapStyle setPenCapStyle(QString&)const;
+    Qt::PenJoinStyle setPenJoinStyle(QString&)const;
+    Qt::GlobalColor setColor(QString&)const;
+    Qt::BrushStyle setBrushStyle(QString&)const;
+    Qt::AlignmentFlag setTextAlignment(QString&)const;
+    //Textpoint size
+    //TextFontFamily
+    QFont::Style getTextFontStyle(QString&)const;
+    QFont::Weight getTextFontWeight(QString&)const;
+private:
     int shapeID=0;
     ShapeType shape;
     custom::vector<QPoint> qPointVector;
@@ -36,5 +51,6 @@ public:
 
 
 };
+
 
 #endif // SHAPEBUFFER_H
