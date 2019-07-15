abe34f9554f95443aa03923b0cd898be6ec08c9e
oliveryue2-Arthur Siu <51488740+oliveryue2@users.noreply.github.com>
2019-07-15 09:52:32 -0700

    Recommit of shapebuffer work
    Previous merge wiped out my work for some reason....

--- 2DGraphicsModeler/shapebuffer.h
+++ 2DGraphicsModeler/shapebuffer.h
@@ -5,8 +5,13 @@
 #include <QBrush>
 #include <QPen>
 #include <QFont>
+#include <QTextStream>
 
-enum ShapeType{LINE,POLYLINE, POLYGON,RECTANGLE,ELLIPSE, TEXT};
+enum ShapeType{LINE, POLYLINE, POLYGON, RECTANGLE,\
+               SQUARE, ELLIPSE, CIRCLE, TEXT};
+
+class MyException
+{};
 
 class ShapeBuffer
 {
@@ -14,12 +19,13 @@ class ShapeBuffer
 public:
     ShapeBuffer();
 
-
     void reset();
     void defaultStyle();
 
     //buffer variables
+    int ShapeID=0;
     ShapeType shape;
+
     custom::vector<QPoint> qPointVector;
     QRect qRect;
     QPolygon qPolygon;
@@ -28,7 +34,20 @@ public:
     QPen pen;
     QFont font;
 
+       void readIn(QTextStream&);
 
+    ShapeType setShape(QString&) const;
+    //Pen Width
+    Qt::PenStyle setPenStyle(QString&)const;
+    Qt::PenCapStyle setPenCapStyle(QString&)const;
+    Qt::PenJoinStyle setPenJoinStyle(QString&)const;
+    Qt::GlobalColor setColor(QString&)const;
+    Qt::BrushStyle setBrushStyle(QString&)const;
+    Qt::AlignmentFlag setTextAlignment(QString&)const;
+    //Textpoint size
+    //TextFontFamily
+    QFont::Style getTextFontStyle(QString&)const;
+    QFont::Weight getTextFontWeight(QString&)const;
 };
 
 #endif // SHAPEBUFFER_H
