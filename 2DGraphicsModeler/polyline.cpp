97e159daa129aee747ea74d32d4440c13700a015
Matin Ghaffari <matinghaffari@yahoo.com>
2019-07-15 17:19:10 -0700

    polyline Finished needs testing

--- 2DGraphicsModeler/polyline.cpp
+++ 2DGraphicsModeler/polyline.cpp
@@ -2,80 +2,22 @@
 #include "vector.h"
 
 
-
-
-PolyLine::PolyLine(QPaintDevice *parent): Shape(parent)
+PolyLine::PolyLine(QPaintDevice *parent, ShapeBuffer arg): Shape(parent, arg)
 {
+    qPolygon = arg.qPolygon;
 
-//Polyline::Polyline(QPaintDevice *parent, ShapeBuffer arg): Shape(parent, arg)
-//{
-//    poly = arg.qPolygon;
-//}
-}
-
-
-PolyLine::PolyLine(QPaintDevice *parent, ShapeType arg,\
-           QPen rhsPen, QBrush rhsBrush):\
-        Shape(parent,arg,rhsPen,rhsBrush)
-
-{
-
-
-
-Polyline::Polyline(QPaintDevice *parent, ShapeBuffer arg): Shape(parent, arg)
-{
- //   qPolygon = arg.qPolygon;
-test=4;
- //QPolygonpush_back(point);
 }
 
 
 PolyLine::~PolyLine()
-
 {
 
 }
 
-void PolyLine::setDimension(int, int)
-{
-   // shapeDimensions.push_back(QPoint(x,y));
-}
-
- void PolyLine::draw(const int, const int)
+void PolyLine::draw(const int, const int)
 {
     QPainter& painter = getQPainter();
     painter.drawPolyline(qPolygon);
-}
-
-void PolyLine::move(const int x, const int y)
-void polyLine::draw(const int x, const int y)
-{
-    QPainter& painter = getQPainter();
-
-
-//        poly << QPoint(0, 85) << QPoint(75, 75)
-//             << QPoint(100, 10) << QPoint(125, 75)
-//             << QPoint(200, 85) << QPoint(150, 125);
-
-   painter.drawPolyline(poly);
-
-}
-
-void polyLine::move(int x, int y)
-{
-    bool allowMove = true;
-    for (int i = 0; i < poly.size(); i++)
-    {
-        if ((poly.point(i).rx() + x) > 1000 || (poly.point(i).ry() + y > 500))
-        {
-            allowMove = false;
-        }
-    }
-
-    if (allowMove)
-    {
-        poly.translate(x,y);
-    }
 }
 
 double PolyLine::area() const
@@ -83,40 +25,36 @@ double PolyLine::area() const
     return 0;
 }
 
+void PolyLine::move(int x, int y)
+{
+    bool allowMove = true;
+    for (int i = 0; i < qPolygon.size(); i++)
+    {
+        if ((qPolygon.point(i).x() + x) > 1000 || (qPolygon.point(i).y() + y > 500))
+        {
+            allowMove = false;
+        }
+    }
+
+    if (allowMove)
+    {
+        qPolygon.translate(x,y);
+    }
+}
+
+
 double PolyLine::perimeter() const
-{
-    return 0;
-
-}
-
-void Polyline::draw(const int, const int)
-{
-
-
-}
-
-void Polyline::move(const int, const int)
-{
-
-double polyLine::perimeter() const
 {
     double perimeter;
     perimeter = 0;
-    for (int i = 0; i < poly.size(); i++)
+    for (int i = 0; i < qPolygon.size(); i++)
     {
-        perimeter += sqrt((((poly.point((i + 1) % poly.size()).rx()) - poly.point(i).rx())
-                         * ((poly.point((i + 1) % poly.size()).rx()) - poly.point(i).rx()))
-                        + (((poly.point((i + 1) % poly.size()).ry()) - poly.point(i).ry())
-                         * ((poly.point((i + 1) % poly.size()).ry()) - poly.point(i).ry())));
+        perimeter += sqrt((((qPolygon.point((i + 1) % qPolygon.size()).rx()) - qPolygon.point(i).rx())
+                         * ((qPolygon.point((i + 1) % qPolygon.size()).rx()) - qPolygon.point(i).rx()))
+                        + (((qPolygon.point((i + 1) % qPolygon.size()).ry()) - qPolygon.point(i).ry())
+                         * ((qPolygon.point((i + 1) % qPolygon.size()).ry()) - qPolygon.point(i).ry())));
     }
     return perimeter;
 }
-
-
-//Polyline::Polyline(QPaintDevice *parent, ShapeBuffer arg): Shape(parent, arg)
-//{
-
-//    qPointArray = new QPoint[3];
-//}
 
 
--- 2DGraphicsModeler/polyline.h
+++ 2DGraphicsModeler/polyline.h
@@ -9,10 +9,8 @@ class PolyLine: public Shape
 {
 public:
     PolyLine() = delete;
-    PolyLine (QPaintDevice *parent);
 
-    PolyLine(QPaintDevice *parent, ShapeType arg, QPen rhsPen, QBrush rhsBrush);
-    PolyLine(QPaintDevice *parent, ShapeType arg);
+    PolyLine(QPaintDevice *parent, ShapeBuffer arg);
 
     virtual ~PolyLine()override;
 
@@ -25,33 +23,8 @@ public:
 
 private:
     QPolygon qPolygon;
-    int test;
-    QPoint *points;
-    int pointsCounter;
-    QPolygon poly;
-
 };
 
 
-
-
-//#include "shape.h"
-
-//class Polyline : public Shape
-//{
-//public:
-//    Polyline()=delete;
-//    Polyline(QPaintDevice * parent,ShapeBuffer );
-//    void draw(const int, const int) override;
-//    void move(const int x, const int y)override;
-//    double area() const override;
-//    double perimeter() const override;
-
-
-//private:
-//    custom::vector<QPoint> qPointVector;
-
-//    QPoint* qPointArray;
-//};
 
 #endif // POLYLINE_H
