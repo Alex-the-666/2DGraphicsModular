28c274eaed4d4a520781e8ced2c0ce7be0ef85fa
Dylan <drossi517@gmail.com>
2019-07-18 07:35:12 -0700

    Updated shape.h with comments for Doxygen

--- 2DGraphicsModeler/shape.h
+++ 2DGraphicsModeler/shape.h
@@ -7,20 +7,57 @@
 #include <cmath>
 #include "shapebuffer.h"
 
+//!     Base Class Shape
+/*!   Shape is the base class for the shape objects-
+*     all other shapes are derived from this class.
+*     contains Pure virtual functions.
+*     Abstract Base Class.
+*/
 class Shape
 {
 public:
+
+      //!Delete the object created by the default constructor
     Shape() = delete;
+     //!Delete the object created by the default copy constructor
     Shape(Shape&) = delete;
+
+    //!Delete the object created by the default assignment operator
+    //!Allows for operator overloading in derived classes
     Shape& operator =(Shape&)= delete;
 
+    //!Pure virtual function
     Shape(QPaintDevice *): shapeId {0} {}
+
+    //!   Shape Class Constructor
+    /*!   Constructor for the shape object
+    *     \param &buffer is a ShapeBuffer object
+    *     passed by const ref.
+    */
     Shape(const ShapeBuffer&);
 
+    /*!    Overloaded equality operator: returns true if
+    *       shapeId == the shapeId of the passed paramater
+    *       \param rhs is a shape object passed by reference
+    */
     bool operator ==(Shape& rhs){return (shapeId==rhs.shapeId);}
+
+    /*!    Overloaded less than operator: returns true if
+    *      shapeId <= the shapeId of the passed paramater
+    *      \param rhs is a shape object passed by reference
+    */
     bool operator <(Shape& rhs){return (shapeId<=rhs.shapeId);}
+
+    /*!    Overloaded greater than operator: returns true if
+    *      shapeId > the shapeId of the passed paramater
+    *      \param rhs is a shape object passed by reference
+    */
     bool operator >(Shape& rhs) {return (area()>rhs.area());}
 
+    /*!    Get function returns a string, depending on the pen.style
+    *      function verifies that the style is a valid QT style,
+    *      and returns a string of the style
+    */
     QString getPenStyle()
     {
         if(pen.style()==Qt::SolidLine)
@@ -56,31 +93,73 @@ public:
 //            else if(getPen().joinStyle()==Qt::RoundJoin)
 //                return "RoundJoin";
 //        }
+
+    //!   Shape Class Destructor
     virtual ~Shape();
 
+    //!    Get function to return a shape object
     ShapeType getShape() const;
+
+    //!    Get function to return a brush object
     const QBrush& getBrush()const;
+
+    //!    Get function to return a pen object
     const QPen& getPen()const;
     void passQPainter(QPainter* arg ){painter=arg;}
 
+    //!    Set function to assign a shape object
+    /*!    Takes a shapetype obj, assigns it to Shape
+    *     \param arg is a ShapeType object
+    */
     void setShape(ShapeType arg);
+
+    //!    Set function to assign a pen object
+    /*!    Takes QT styles and assigns them to the pen object
+    *     \param gc is a QT style to set the color of the pen
+    *     \param width is an int variable to set the width of the pen
+    *     \param ps is a QT style to set the pen style
+    *     \param pcs is a QT style to set the pen cap style
+    *     \param pjs is a QT style to set the join style of the pen
+    */
     void setPen(Qt::GlobalColor gc1, int width,\
                  Qt::PenStyle ps, Qt::PenCapStyle pcs, Qt::PenJoinStyle pjs);
+
+      //!    Set function to assign to a brush object
+      /*!   Takes QT styles as paramaters and assigns them to the brush object
+      *     \param gc is a QT style for global color- sets the brush color
+      *     \param bs is a QT style to set the brush style
+      */
     void setBrush(Qt::GlobalColor gc2, Qt::BrushStyle bs);
 
+    //!Pure virtual function- allows for operator overloading of derived classes
     virtual void draw(const int x, const int y) = 0;
+
+    //!Pure virtual function- allows for operator overloading of derived classes
     virtual void draw()=0;
+
+    //!Pure virtual function- allows for operator overloading of derived classes
     virtual void move(const int x,const int y) = 0;
+
+    //!Pure virtual function- allows for operator overloading of derived classes
     virtual double area() const = 0;
+
+    //!Pure virtual function- allows for operator overloading of derived classes
     virtual double perimeter() const =0;
 protected:
+
+      //!     Function returns *this pointer, a pointer of type QPainter
     QPainter* getQPainter();
 
 private:
+    //! A pointer of type QPainter
     QPainter* painter;
+    //! int shapeId assigns shape based on value
     int shapeId;
+    //! Shapetype object created, passed to derived classes and shapeBuffer
     ShapeType shape;
+    //! QPen object to set the pen styles
     QPen pen;
+    //! QBrush object to set the brush styles     
     QBrush brush;
 };
 
