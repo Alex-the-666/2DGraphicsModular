9cd8091315b7b0724a5af8bc9ed39573545a2f92
Dylan <drossi517@gmail.com>
2019-07-19 14:21:49 -0700

    Doxygen done

--- 2DGraphicsModeler/circle.h
+++ 2DGraphicsModeler/circle.h
@@ -2,9 +2,9 @@
 #define CIRCLE_H
 #include "ellipse.h"
 
-//!    Derived Class: Circle
-/*!   Circle is a derived class of the ellipse class-
-*     Inherits private variables from protected class of ellipse
+//!    Derived Class From Base Class Ellipse
+/*!   Circle is a derived class of the ellipse class.\n
+*     Inherits private variables from protected class of ellipse.\n
 *     Circle can be styled with different pens, colors, and brushes.
 */
 class Circle : public Ellipse
@@ -13,59 +13,54 @@ public:
       //!Default circle class constructor
       /*!Function calls default base class constructor
       * Base Class Constructor calls setShape() and assigns
-      * enum value of CIRCLE- passes object to a Circle object.
+      * enum value of CIRCLE- passes object to a Circle object.\n
+      * Passes int values for generic size of object.
       */
     Circle():Ellipse(100, 100, 100, 100) {setShape(CIRCLE);}
 
     //!   Circle Class Copy Constructor
-    /*!   Copy Constructor for the circle object
+    /*!   Copy Constructor for the circle object.\n
+    *     Parameter passed by const reference.
     *     \param &arg sets both radius variables to be the same- creating a circle.
-    *     passed by const ref.
     */
     Circle(const ShapeBuffer&);
 
     //!  Virtual Circle Class Destructor
     ~Circle() override {}
 
-    //!   Setter Function that sets the ShapeBuffer to an ellipse object
+    //!   Setter Function that sets the ellipse to a ShapeBuffer object
     /*!   function that is passed a ShapeBuffer Object by reference
     *     \param temp sets the new rectangle objects values used in creation of ellipse/circle
     */
     void setShapeBuffer(ShapeBuffer&)override;
 
-    //!   Setter Function that sets the circle objects dimensions
-    /*!   \param int  is the x axis value
-    *     \param int  is the y axis value
-    *     \param int  the width int value
-    *     \param int  the height int value
-    *     inline, empty function
-    */
+
     //this function has no definition- only a prototype
-    void setDimension(int , int, int, int){}
+    /*void setDimension(int , int, int, int){}*/
 
     //!   Function that draws a Circle
-    /*!   Sets the QPainter objects pen
-    *     Sets the QPainter objects brush
-    *     Sets the QPainter coordinates from which to draw a circle
+    /*!   Sets the QPainter objects pen.\n
+    *     Sets the QPainter objects brush.\n
+    *     Sets the QPainter coordinates from which to draw a circle.\n
     *     \param x is the left-most coordinate of the object
     *     \param y is the up-most coordinate of the object
     */
     void draw(const int x, const int y) override;
 
     //!   Function that draws an Circle
-    /*!   Sets the QPainter objects pen
-    *     Sets the QPainter objects brush
-    *     Sets the QPainter coordinates from which to draw a circle
-    *     calls the drawID function - which assigns an ID #
-    *     calls passQPainter function and passes a nullptr
+    /*!   Sets the QPainter objects pen.\n
+    *     Sets the QPainter objects brush.\n
+    *     Sets the QPainter coordinates from which to draw a circle.\n
+    *     Calls the drawID function - which assigns an ID #.\n
+    *     Calls passQPainter function and passes a nullptr.
     */
     void draw()override;
 
     //!   Function that moves a Circle
     /*!   Checks to make sure that the new coordinates that are
-    *     passed as parameters does not force the object off the screen.
+    *     passed as parameters does not force the object off the screen.\n
     *     If it passes the logic check, the passed parameters are saved
-    *     as new values
+    *     as new values.
     *     \param x is the x-axis value of the top left corner of the object
     *     \param y is the y-axis value of the top left corner of the object
     */
