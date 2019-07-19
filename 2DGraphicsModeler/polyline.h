bfb342bab2e41e75832d2f4b9be16ec6df317054
Dylan <drossi517@gmail.com>
2019-07-19 16:09:47 -0700

    Update polyline.h

--- 2DGraphicsModeler/polyline.h
+++ 2DGraphicsModeler/polyline.h
@@ -7,8 +7,9 @@
 *  \subsection intro_sec Introduction
 *
 * This product is a 2D Graphic Modeller that can create a variety of shapes and text objects.
-* This program can read in from a text file, and save to a text file.
-* The objects that appear on screen can have their: location, color, border, and text modified.
+* This program can read in from a text file, and save to a text file.\n
+* The objects that appear on screen can have their: location, color, border, and text modified.\n\n\n
+* Created by: Alexander Rownlands, Arthur Siu, Blake Del Rey, Dylan Rossi, Matin Ghaffari, Sina Havaeeahari, and William Duong.
 */
 
 //!    Derived Class From Base Class Shape
