f6ff5a68cb8df4833727c847bb657c183b38e5c3
sina <sin30ha@gmail.com>
2019-07-20 12:23:45 -0700

    some memory leaks :|

--- 2DGraphicsModeler/shapeinfo.cpp
+++ 2DGraphicsModeler/shapeinfo.cpp
@@ -19,8 +19,7 @@ ShapeInfo::ShapeInfo(QWidget *parent) :
     setWindowTitle(tr("Shape Info"));
     MainWindow* w = dynamic_cast<MainWindow*>(this->parentWidget());
     custom::vector<Shape*>& shp = w->renderArea ->getShapeVector();
-    QTreeWidgetItem *item;
-
+    QTreeWidgetItem *item = new QTreeWidgetItem;
 
     for(auto i = shp.begin(); i != shp.end(); i++)
     {
@@ -53,6 +52,8 @@ ShapeInfo::ShapeInfo(QWidget *parent) :
         }
     }
     //ui->treeWidget->setColumnCount(1);
+    delete item;
+    item = nullptr;
 }
 
 ShapeInfo::~ShapeInfo()
